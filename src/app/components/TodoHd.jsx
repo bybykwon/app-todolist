f028d372d45465efac3862dc19563389fb0b20fb
bybykwon <byoungyoonkwon@google.com>
2024-11-22 15:41:31 +0900

    dd

--- src/app/components/TodoHd.jsx
+++ src/app/components/TodoHd.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+// 오늘 날짜 표시
 
+import React from 'react';
+import * as dateFns from 'date-fns';
 const TodoHd = () => {
     return (
         <div>
-            <h2>2024.04.22</h2>
-            <p>오늘 할 일을 적어보세요.</p>
+            <h2 className='text-2xl'>{dateFns.format(new Date(), 'yyyy.MM.dd')}</h2>
+            <p className='text-2xl'>오늘 할 일을 적어보세요.</p>
         </div>
     );
 };
